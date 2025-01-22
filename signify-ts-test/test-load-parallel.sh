2f06469e7705485afa96b8604276620d3bc8271e
Ronak Seth <ronakseth@Ronaks-MacBook-Pro.local>
2025-01-22 11:51:40 -0500

    Staging for api test

--- signify-ts-test/test-load-parallel.sh
+++ signify-ts-test/test-load-parallel.sh
@@ -199,7 +199,7 @@ validate_inputs() {
 }
 
 check_available_banks() {
-    local TOTAL_AVAILABLE_BANKS=601
+    local TOTAL_AVAILABLE_BANKS=750
 
     if (( BANK_COUNT + FIRST_BANK - 1 > TOTAL_AVAILABLE_BANKS )); then
         echo "WARNING: You have selected more banks ($((BANK_COUNT + FIRST_BANK - 1))) than available ($TOTAL_AVAILABLE_BANKS)."
